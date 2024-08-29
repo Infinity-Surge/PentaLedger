a0e214c5eb9953a489ddd6abfeb6df8caf7667b0
Joe Turner <joe@agavemountain.com>
2024-08-29 12:25:23 -0500

    configure port via commandline

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -14,8 +19,8 @@ func main() {
 
 	mux.HandleFunc("GET /{$}", home)
 
-	log.Print("Starting server on :4000")
+	log.Print("Starting server on ", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
